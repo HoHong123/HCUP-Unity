9676d564ee36c545fbdcec3da5f73dd950c28800
PKH <epzmfhs@gmail.com>
2026-03-16 00:13:43 +0900

    [Feat] 🆕 16.03.26 'HUtil.Pooling' 업데이트.

--- HoHong123/HUtil/Runtime/HUtil/Pooling/ClassPool.cs
+++ HoHong123/HUtil/Runtime/HUtil/Pooling/ClassPool.cs
@@ -1,23 +1,54 @@
+﻿#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ * 일반 C# 클래스 타입을 대상으로 사용하는 풀링 클래스입니다.
+ *
+ * 특징 ::
+ * - new() 제약을 사용하는 클래스 풀링
+ * - BasePool을 기반으로 동작
+ * - 객체 생성 시 자동으로 onCreate 이벤트 호출
+ * =========================================================
+ */
+#endif
+
 using System;
 
 namespace HUtil.Pooling {
     public class ClassPool<T> : BasePool<T> where T : class, new() {
+        #region Public - Constructors
         public ClassPool(int initSize = 1,
             Action<T> onCreate = null, Action<T> onGet = null,
             Action<T> onReturn = null, Action<T> onDispose = null)
             : base(onCreate, onGet, onReturn, onDispose)
             => Init(initSize);
+        #endregion
 
+        #region Protected - Create
         protected override T Create() {
             var newOne = new T();
             onCreate?.Invoke(newOne);
             return newOne;
         }
+        #endregion
     }
 }
 
 #if UNITY_EDITOR
-/* @Jason - PKH
- * 
+/* =========================================================
+ * @Jason - PKH
+ *
+ * 주요 기능 ::
+ * new() 제약 기반 클래스 풀링
+ *
+ * 생성 방식 ::
+ * new T()
+ *
+ * 사용법 ::
+ * var pool = new ClassPool<MyClass>(10);
+ * var obj = pool.Get();
+ *
+ * 기타 ::
+ * 일반 C# 객체 풀링 전용 클래스입니다.
+ * =========================================================
  */
 #endif
--- HoHong123/HUtil/Runtime/HUtil/Pooling/ComponentPool.cs
+++ HoHong123/HUtil/Runtime/HUtil/Pooling/ComponentPool.cs
@@ -1,12 +1,31 @@
+﻿#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ * Unity Component 타입을 대상으로 사용하는 풀링 클래스입니다.
+ *
+ * 특징 ::
+ * - 특정 Component 타입을 기준으로 풀링
+ * - Prefab 기반 생성 지원
+ * - 부모 Transform 지정 가능
+ *
+ * 생성 방식 ::
+ * 1. prefab이 존재할 경우 Instantiate
+ * 2. prefab이 없을 경우 GameObject + Component 생성
+ * =========================================================
+ */
+#endif
+
 using System;
 using UnityEngine;
 
 namespace HUtil.Pooling {
     public class ComponentPool<T> : BasePool<T> where T : Component {
+        #region Fields
         private readonly T prefab;
         private readonly Transform parent;
+        #endregion
 
-
+        #region Public - Constructors
         public ComponentPool(
             T prefab, int initialSize = 5, Transform parent = null,
             Action<T> onCreate = null, Action<T> onGet = null,
@@ -16,8 +35,9 @@ namespace HUtil.Pooling {
             this.parent = parent;
             Init(initialSize);
         }
+        #endregion
 
-
+        #region Protected - Create
         protected override T Create() {
             GameObject obj;
             if (prefab) {
@@ -31,11 +51,30 @@ namespace HUtil.Pooling {
             onCreate?.Invoke(component);
             return component;
         }
+        #endregion
     }
 }
 
 #if UNITY_EDITOR
-/* @Jason - PKH
- * 
+/* =========================================================
+ * @Jason - PKH 2026.03.10
+ *
+ * 주요 기능 ::
+ * Component 기반 풀링
+ *
+ * 생성 방식 ::
+ * prefab 존재 → Instantiate
+ * prefab 없음 → GameObject + Component 생성
+ *
+ * 옵션 ::
+ * parent
+ *  + 생성된 객체 부모 지정
+ *
+ * 사용법 ::
+ * var pool = new ComponentPool<MyComponent>(prefab);
+ *
+ * 기타 ::
+ * MonoBehaviour 기반 풀링을 지원합니다.
+ * =========================================================
  */
 #endif
