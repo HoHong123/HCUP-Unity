7998704004093b92e1fe74cef806009c4dc6d2ed
PKH <epzmfhs@gmail.com>
2026-03-16 00:13:06 +0900

    [Feat] 🆕 16.03.26 'HUtil.Inspector' 업데이트.

--- HoHong123/HUtil/Runtime/HUtil/Inspector/HBoxGroupAttribute.cs
+++ HoHong123/HUtil/Runtime/HUtil/Inspector/HBoxGroupAttribute.cs
@@ -1,3 +1,18 @@
+﻿#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ * Inspector에서 필드를 같은 그룹으로 묶기 위한 Attribute입니다.
+ * 동일한 GroupName을 가진 필드를 하나의 Horizontal Box 그룹으로 정렬합니다.
+ *
+ * 사용 예 ::
+ * [HBoxGroup("Stats")]
+ * public int hp;
+ * [HBoxGroup("Stats")]
+ * public int atk;
+ * =========================================================
+ */
+#endif
+#if UNITY_EDITOR
 namespace HUtil.Inspector {
     [System.AttributeUsage(System.AttributeTargets.Field, AllowMultiple = true, Inherited = true)]
     public class HBoxGroupAttribute : HInspectorAttribute {
@@ -9,3 +24,4 @@ namespace HUtil.Inspector {
         }
     }
 }
+#endif
