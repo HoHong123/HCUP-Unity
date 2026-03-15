5b720d39efc2eb67c6c6227db92d347459b7539b
PKH <epzmfhs@gmail.com>
2026-03-16 00:13:56 +0900

    [Feat] 🆕 16.03.26 'HUtil.Primitives' 업데이트.

--- HoHong123/HUtil/Runtime/HUtil/Primitives/StringUtil.cs
+++ HoHong123/HUtil/Runtime/HUtil/Primitives/StringUtil.cs
@@ -1,7 +1,15 @@
-#if UNITY_EDITOR
+﻿#if UNITY_EDITOR
 /* =========================================================
  * @Jason - PKH
- * string 추가 기능 스크립트입니다.
+ * 문자열 관련 확장 기능을 제공하는 유틸리티 클래스입니다.
+ *
+ * 기능 ::
+ * - Query String 파싱
+ * - Base64 인코딩 / 디코딩
+ * - 숫자 포맷팅
+ * - 숫자 단위 축약 표시
+ * - 시간 포맷 변환
+ * - 문자열 필터링
  * =========================================================
  */
 #endif
@@ -12,6 +20,7 @@ using System.Collections.Generic;
 
 namespace HUtil.Formattable {
     public static class StringUtil {
+        #region Parsing Query
         public static Dictionary<string, string> ParseQueryString(this string query) {
             Dictionary<string, string> queryParams = new Dictionary<string, string>();
 
@@ -28,7 +37,9 @@ namespace HUtil.Formattable {
 
             return queryParams;
         }
+        #endregion
 
+        #region Base64 Parsing
         public static string EncodeStringToBase64(this string plainText) {
             byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
             string base64EncodedData = Convert.ToBase64String(plainTextBytes);
@@ -40,7 +51,9 @@ namespace HUtil.Formattable {
             string decodedString = Encoding.UTF8.GetString(decodedBytes);
             return decodedString;
         }
+        #endregion
 
+        #region Format Numbers to String
         /// <summary>
         /// Formats the given formattable value by inserting a comma(',')
         /// as a thousands separator every three digits.
@@ -74,7 +87,9 @@ namespace HUtil.Formattable {
 
             return num.ToString("0");
         }
+        #endregion
 
+        #region Format Numbers to Time
         /// <summary>Seconds => 'HH:MM:SS' (Less than 1 hour format to 'MM:SS')</summary>
         public static string ToClock(this float seconds) {
             int tot = UnityEngine.Mathf.Max(0, UnityEngine.Mathf.FloorToInt(seconds));
@@ -83,5 +98,64 @@ namespace HUtil.Formattable {
             int ss = tot % 60;
             return (hh > 0) ? $"{hh:00}:{mm:00}:{ss:00}" : $"{mm:00}:{ss:00}";
         }
+
+        public static string ToClock(this TimeSpan remain) {
+            float seconds = (float)Math.Max(0.0, remain.TotalSeconds);
+            return seconds.ToClock();
+        }
+        #endregion
+
+        #region Filter
+        public static string FilterText(this string src, string key) {
+            if (string.IsNullOrWhiteSpace(key)) return src;
+
+            var lines = src.Split('\n');
+            var sb = new StringBuilder();
+            foreach (var k in lines) {
+                if (k.IndexOf(key, StringComparison.OrdinalIgnoreCase) >= 0)
+                    sb.AppendLine(k);
+            }
+
+            return sb.ToString();
+        }
+        #endregion
+
+        #region Check Uni-NewLine
+        public static bool IsMultiLine(this string value) {
+            if (string.IsNullOrEmpty(value)) return false;
+            if (value.Contains('\n')) return true; // Unix
+            if (value.Contains('\r')) return true; // 구형 Mac, 기타
+            return false;
+        }
+        #endregion
     }
 }
+
+#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ *
+ * 주요 기능 ::
+ * QueryString 파싱
+ * Base64 문자열 변환
+ * 숫자 포맷팅
+ * 숫자 단위 축약 표시
+ * 시간 포맷 변환
+ * 문자열 필터링
+ *
+ * 주요 API ::
+ * ParseQueryString
+ * FormatNumber
+ * NumToAlpha
+ * ToClock
+ * FilterText
+ *
+ * 사용법 ::
+ * 1000000.NumToAlpha()
+ * 90f.ToClock()
+ *
+ * 기타 ::
+ * 프로젝트 전반에서 사용하는 문자열 유틸리티 모음입니다.
+ * =========================================================
+ */
+#endif
