a206796c50addb054f4ec87f6e9e24a8f39d4a49
PKH <epzmfhs@gmail.com>
2026-03-16 00:13:17 +0900

    [Feat] 🆕 16.03.26 'HUtil.Logger' 업데이트.

---
+++ HoHong123/HUtil/Runtime/HUtil/Logger/LogLevel.cs
@@ -0,0 +1,18 @@
+﻿#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ * 로그 시스템에서 사용하는 로그 레벨 열거형입니다.
+ * =========================================================
+ */
+#endif
+
+namespace HUtil.Logger {
+    public enum LogLevel : byte {
+        Debug,
+        Log,
+        Warn,
+        Error,
+        Fatal,
+        Assert,
+    }
+}
