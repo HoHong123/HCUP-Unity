2dfc87599782b36c646dc458d845d56cfd41dd37
PKH <epzmfhs@gmail.com>
2026-03-16 00:08:05 +0900

    [Feat] 🆕 16.03.26 'HUI.Scrollview' 업데이트.

---
+++ HoHong123/HUI/Runtime/HUI/Scrollview/Demo/DemoCellData.cs
@@ -0,0 +1,9 @@
+using HUI.ScrollView;
+
+public class DemoCellData : BaseRecycleCellData {
+    public string tester;
+
+    public DemoCellData(string tester) {
+        this.tester = tester;
+    }
+}
