54683c4b1342557cc4b1c5cb50db965c6581d7e2
PKH <epzmfhs@gmail.com>
2026-03-16 00:13:43 +0900

    [Feat] 🆕 16.03.26 'HUtil.Pooling' 업데이트.

--- HoHong123/HUtil/Runtime/HUtil/Pooling/GameObjectPool.cs
+++ HoHong123/HUtil/Runtime/HUtil/Pooling/GameObjectPool.cs
@@ -1,11 +1,27 @@
+﻿#if UNITY_EDITOR
+/* =========================================================
+ * @Jason - PKH
+ * GameObject를 대상으로 사용하는 풀링 클래스입니다.
+ *
+ * 특징 ::
+ * - Prefab 기반 GameObject 풀링
+ * - 부모 Transform 지정 가능
+ * - BasePool 구조를 기반으로 동작
+ * =========================================================
+ */
+#endif
+
 using System;
 using UnityEngine;
 
 namespace HUtil.Pooling {
     public class GameObjectPool : BasePool<GameObject> {
+        #region Fields
         readonly GameObject prefab;
         readonly Transform parent;
+        #endregion
 
+        #region Public - Constructors
         public GameObjectPool(
             GameObject prefab, int initialSize = 5, Transform parent = null,
             Action<GameObject> onCreate = null, Action<GameObject> onGet = null,
@@ -15,17 +31,37 @@ namespace HUtil.Pooling {
             this.parent = parent;
             Init(initialSize);
         }
+        #endregion
 
+        #region Protected - Create
         protected override GameObject Create() {
             var obj = GameObject.Instantiate(prefab, parent);
             onCreate?.Invoke(obj);
             return obj;
         }
+        #endregion
     }
 }
 
 #if UNITY_EDITOR
-/* @Jason - PKH
- * 
+/* =========================================================
+ * @Jason - PKH 2026.03.10
+ *
+ * 주요 기능 ::
+ * GameObject Prefab 풀링
+ *
+ * 생성 방식 ::
+ * Instantiate(prefab)
+ *
+ * 옵션 ::
+ * parent
+ *  + 생성된 GameObject 부모 지정
+ *
+ * 사용법 ::
+ * var pool = new GameObjectPool(prefab, 10);
+ *
+ * 기타 ::
+ * 가장 기본적인 Unity 오브젝트 풀링 구현입니다.
+ * =========================================================
  */
 #endif
