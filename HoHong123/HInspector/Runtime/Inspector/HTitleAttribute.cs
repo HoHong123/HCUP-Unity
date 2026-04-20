8f8efa38b1f53e74e0356163ae900335ccb900de
JasonCatze <epzmfhs@gmail.com>
2026-04-20 18:28:25 +0900

    [Refact] ♻️ 20.04.26 HTitle을 System.Attribute 직접 상속으로 변경 (PropertyAttribute 검증 우회해 브릿지 매핑 정상화)

--- HoHong123/HInspector/Runtime/Inspector/HTitleAttribute.cs
+++ HoHong123/HInspector/Runtime/Inspector/HTitleAttribute.cs
@@ -29,13 +29,18 @@
 #endif
 
 namespace HInspector {
+    // HTitle은 HInspectorAttribute 계열에서 제외되어 System.Attribute를 직접 상속한다.
+    // 이유: HInspectorAttribute는 UnityEngine.PropertyAttribute 파생이라 Odin이 "Field 전용"으로 사전 검증.
+    // 메서드에 [HTitle]이 붙으면 Odin 에러, 필드에 붙어도 PropertyDrawer 경로로 우선 떨어져 번역 누락이 발생한다.
+    // HButton / HShowInInspector와 동일하게 System.Attribute 직접 상속으로 이 검증을 우회해야 브릿지가 정상 번역하고 Method/Field/POCO 내부 모두에서 작동한다.
     [System.Diagnostics.Conditional("UNITY_EDITOR")]
     [System.AttributeUsage(System.AttributeTargets.All, AllowMultiple = true, Inherited = true)]
-    public class HTitleAttribute : HInspectorAttribute {
+    public class HTitleAttribute : System.Attribute {
         public string Title { get; }
-        public HTitleAttribute(string title, int order = -50)
-            : base(order) {
+        public int Order { get; }
+        public HTitleAttribute(string title, int order = -50) {
             Title = title;
+            Order = order;
         }
     }
 }
